dbfcae3c1514f4f964cb4e6c550797ca4abfe974
Muhammad Rifky Affandi <rifky.affandi7@gmail.com>
2018-08-08 09:16:28 +0700

    fix detail berita

--- src/Container/SubBerita/DetailBerita.jsx
+++ src/Container/SubBerita/DetailBerita.jsx
@@ -8,18 +8,33 @@ export default class DetailBerita extends Component {
 
   componentDidMount() {
     axios
+      // akan mengambil .params.slug
       .get(
         `https://id.techinasia.com/wp-json/techinasia/3.0/posts/${
-          this.props.match.params
+          this.props.match.params.slug
         }`
       )
-      .then();
+      .then(res => {
+        console.log(res, ">>>>>>> ini resss");
+        this.setState({
+          data: res.data,
+          loading: true
+        });
+      });
   }
   render() {
-    console.log(this.props.match, "ini kiee >>>>>>>>>>>>>.");
+    console.log(this.state.data, "ini kiee >>>>>>>>>>>>>."); //setiap Route pasti ada match
     return (
       <div>
-        <h1>INI SUB BERITA!!!</h1>
+        {this.state.loading ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: this.state.data.posts[0].content //__html => inner html. semua tag nya akan di tampilkan.. post[0]
+            }}
+          />
+        ) : (
+          <h1> Loading sub Berita ... </h1>
+        )}
       </div>
     );
   }
